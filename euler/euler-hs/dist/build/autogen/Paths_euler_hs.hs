8de8973d2f0fc58d116ba4e83ec1faf5c79ed8f4
Phil Glover <phil@goldshank.co.uk>
2015-10-12 18:41:45 +0100

    remove things that should have been ignored

--- euler/euler-hs/dist/build/autogen/Paths_euler_hs.hs
+++ euler/euler-hs/dist/build/autogen/Paths_euler_hs.hs
@@ -1,30 +1,33 @@
 module Paths_euler_hs (
     version,
     getBinDir, getLibDir, getDataDir, getLibexecDir,
-    getDataFileName
+    getDataFileName, getSysconfDir
   ) where
 
 import qualified Control.Exception as Exception
 import Data.Version (Version(..))
 import System.Environment (getEnv)
+import Prelude
+
 catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
 catchIO = Exception.catch
 
-
 version :: Version
-version = Version {versionBranch = [0,1,0,0], versionTags = []}
-bindir, libdir, datadir, libexecdir :: FilePath
+version = Version [0,1,0,0] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/home/phil/.cabal/bin"
-libdir     = "/home/phil/.cabal/lib/euler-hs-0.1.0.0/ghc-7.4.1"
-datadir    = "/home/phil/.cabal/share/euler-hs-0.1.0.0"
-libexecdir = "/home/phil/.cabal/libexec"
+bindir     = "/home/phil/git/sandbox/euler/euler-hs/.cabal-sandbox/bin"
+libdir     = "/home/phil/git/sandbox/euler/euler-hs/.cabal-sandbox/lib/x86_64-linux-ghc-7.6.3/euler-hs-0.1.0.0"
+datadir    = "/home/phil/git/sandbox/euler/euler-hs/.cabal-sandbox/share/x86_64-linux-ghc-7.6.3/euler-hs-0.1.0.0"
+libexecdir = "/home/phil/git/sandbox/euler/euler-hs/.cabal-sandbox/libexec"
+sysconfdir = "/home/phil/git/sandbox/euler/euler-hs/.cabal-sandbox/etc"
 
-getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "euler_hs_bindir") (\_ -> return bindir)
 getLibDir = catchIO (getEnv "euler_hs_libdir") (\_ -> return libdir)
 getDataDir = catchIO (getEnv "euler_hs_datadir") (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "euler_hs_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "euler_hs_sysconfdir") (\_ -> return sysconfdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
