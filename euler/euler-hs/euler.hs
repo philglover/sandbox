271c91dc4e5ab13b903f904c8545faac67a3ed35
Phil Glover <phil@goldshank.co.uk>
2015-10-12 18:39:12 +0100

    Solution 1

--- euler/euler-hs/euler.hs
+++ euler/euler-hs/euler.hs
@@ -1,5 +1,15 @@
 import System.Environment
+import Data.List
+
+isFactorOf :: Int -> Int -> Bool
+isFactorOf x y = x `mod` y == 0 
+
+solution1 :: String
+solution1 = show ( sum [x | x <- [1..999], x `isFactorOf` 3 || x `isFactorOf` 5] )
+
+solutions :: [String]
+solutions = [solution1]
 
 -- | 'main' runs the main program
-main :: IO ()
-main = getArgs >>= print
+main :: IO()
+main = print( zip [1..] solutions )
