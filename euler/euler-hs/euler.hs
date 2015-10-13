068d031e1fc6f6c4737383cf04dc0a3a3a091202
Phil Glover <phil@goldshank.co.uk>
2015-10-13 09:46:41 +0100

    Solution 2

--- euler/euler-hs/euler.hs
+++ euler/euler-hs/euler.hs
@@ -4,11 +4,22 @@ import Data.List
 isFactorOf :: Int -> Int -> Bool
 isFactorOf x y = x `mod` y == 0 
 
+fibonacci :: (Int -> Bool) -> [Int]
+fibonacci p = fibloop 1 2 p
+fibloop :: Int -> Int -> (Int -> Bool) -> [Int]
+fibloop a b p
+  | p(a + b) = [a, b]
+  | otherwise = [a] ++ fibloop b (a + b) p
+
 solution1 :: String
 solution1 = show ( sum [x | x <- [1..999], x `isFactorOf` 3 || x `isFactorOf` 5] )
 
+solution2 :: String
+solution2Limit = \x -> x >= 4000000
+solution2 = show ( sum [ x | x <- fibonacci solution2Limit, x `isFactorOf` 2] )
+
 solutions :: [String]
-solutions = [solution1]
+solutions = [solution1, solution2]
 
 -- | 'main' runs the main program
 main :: IO()
